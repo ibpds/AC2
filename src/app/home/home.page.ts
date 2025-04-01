597319c7efb731525b4237671beb1ff5f34bec17
ibpds <pordeus.school@gmail.com>
2025-04-01 07:40:49 -0300

    iniciando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +11,46 @@ export class HomePage {
 
   constructor() {}
 
+
+  valorRange = 0;
+  inputValor = '';
+  resGorjeta = '';
+  valorConta = '';
+
+   calcularGorjeta(ev:RangeCustomEvent){
+    this.valorRange = (Number(ev.detail.value.toString())/100);
+    this.resGorjeta = Math.round(Number(this.inputValor)*this.valorRange).toString();
+    this.valorConta = (Number(this.inputValor) + Number(this.resGorjeta)).toString();
+  };
+
+  medidaInicial = '';
+  medidaFinal = '';
+  resMedida = '';
+  cm = '';
+  metro = ''
+  km = '';
+  pes = '';
+  polegadas = '';
+
+
+
+  converterMedidas(ev:RangeCustomEvent){
+    this.valorRange = (Number(ev.detail.value.toString()));
+
+    if(this.medidaInicial == 'Centímetros'){
+      if(this.medidaFinal == 'pes'){
+        this.resMedida = (this.valorRange/30.48).toString();
+      } else if(this.medidaFinal == 'polegadas'){
+        this.resMedida = (this.valorRange*0.3937).toString();
+      }
+    } else if(this.medidaInicial == 'Metros'){
+      if(this.medidaFinal == 'pes'){
+        this.resMedida = (this.valorRange/0.3048).toString();
+      } else if(this.medidaFinal == 'polegadas'){
+        this.resMedida = (this.valorRange*39.37).toString();
+      }
+    } else{
+
+    }
+  }
 }
